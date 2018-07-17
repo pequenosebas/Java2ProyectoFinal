de2519c3a701654a7fb44bc6bfa285dd25acc222
T-101 <T-101@PC235>
2018-07-16 19:49:24 -0500

    Proyecto Final

---
+++ ProyectoFinal1/src/modelo/Usuario.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author T-101
+ */
+public class Usuario {
+    
+    private String login;
+    private String pass;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+    
+    
+}
